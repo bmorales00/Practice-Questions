1d2efb19ea038f7d31109d279a0eef3d198acfc5
Brian <brianmorales395@yahoo.com>
2024-12-02 13:04:20 -0500

    Solved ReverseNodes in python

---
+++ Python/Leetcode_Python/Lists/ReverseNodesGroup.py
@@ -0,0 +1,46 @@
+#Time Complexity: O(n) || Space Complexity: O(n)
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+
+
+def reverseKGroup(head: Optional[ListNode], k: int) -> Optional[ListNode]:
+    if not head:
+        return None
+    if k == 1:
+        return head
+
+    refList = []
+
+    curr = head
+
+    while curr:
+        refList.append(curr)
+        curr = curr.next
+
+    start = 0
+    end = k - 1
+
+    while end < len(refList):
+        tempstart = start
+        tempend = end
+
+        while tempstart < tempend:
+            temp = refList[tempstart]
+            refList[tempstart] = refList[tempend]
+            refList[tempend] = temp
+            tempstart += 1
+            tempend -= 1
+        start += k
+        end += k
+
+    for i in range(len(refList) - 1):
+        refList[i].next = refList[i + 1]
+
+    refList[-1].next = None
+
+    return refList[0]
