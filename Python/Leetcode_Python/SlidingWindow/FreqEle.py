74f98669fe760c9040bb80e25eb9a041da95fb2e
Brian <brianmorales395@yahoo.com>
2025-01-10 16:40:23 -0500

    Changed a comment

--- Python/Leetcode_Python/SlidingWindow/FreqEle.py
+++ Python/Leetcode_Python/SlidingWindow/FreqEle.py
@@ -1,12 +1,11 @@
 # Time Complexity: O(n) || Space Complexity: O(1)
 from typing import List
 
-
-def maxFrequency(nums:List[int], k:int)->int:
+def maxFrequency(nums: List[int], k: int) -> int:
     if not nums:
         return -1
 
-    if len(nums)==1:
+    if len(nums) == 1:
         return 1
 
     nums.sort()
@@ -14,15 +13,13 @@ def maxFrequency(nums:List[int], k:int)->int:
     r = 0
 
     total = nums[l]
-    res  = 1
+    res = 1
     while r < len(nums):
-        if nums[r] * ((r-l)+1) <= total+k:
-            r+=1
-            total+=nums[r]
+        if nums[r] * ((r - l) + 1) <= total + k:
+            r += 1
+            total += nums[r]
         else:
-            total-=nums[l]
-            l+=1
-        res = max(res, (r-l)+1)
+            total -= nums[l]
+            l += 1
+        res = max(res, (r - l) + 1)
     return res
-
-
